baa09cf269c40ee0a8955b779623fdeee16015a7
yejin <jinee0717@daum.net>
2019-12-26 04:20:19 +0900

    [일정등록]뷰 완성

--- app/src/main/java/com/devaon/early_buddy_android/feature/MainActivity.kt
+++ app/src/main/java/com/devaon/early_buddy_android/feature/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.devaon.early_buddy_android.feature
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.devaon.early_buddy_android.R
+import java.security.MessageDigest
 
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_route)
+        setContentView(R.layout.activity_schdule)
     }
 }
