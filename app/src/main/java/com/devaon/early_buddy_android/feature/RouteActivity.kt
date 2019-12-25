b3526a8c9731c8cd60e2d2a5d56c4e55e9df0fde
ghkdua1829 <ghkdua059@gmail.com>
2019-12-25 19:22:45 +0900

    [홈뷰제작]경로액티비티 제작

---
+++ app/src/main/java/com/devaon/early_buddy_android/feature/RouteActivity.kt
@@ -0,0 +1,13 @@
+package com.devaon.early_buddy_android.feature
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.devaon.early_buddy_android.R
+
+class RouteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_route)
+    }
+}
