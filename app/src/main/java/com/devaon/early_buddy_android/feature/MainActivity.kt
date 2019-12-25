14671017a23442446a8cb04dd0bea20d4ea7d54f
ghkdua1829 <ghkdua059@gmail.com>
2019-12-25 19:21:55 +0900

    [홈뷰제작]시작 화면 설정

--- app/src/main/java/com/devaon/early_buddy_android/feature/MainActivity.kt
+++ app/src/main/java/com/devaon/early_buddy_android/feature/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.devaon.early_buddy_android.feature
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.devaon.early_buddy_android.R
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
