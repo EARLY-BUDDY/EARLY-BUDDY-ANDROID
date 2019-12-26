b864a2cdbb3e45ea7bdbc87fdea5b181ddaa6202
devAon <yewon9742@gmail.com>
2019-12-26 18:18:53 +0900

    [일정등록]장소선택 뷰, 어댑터, 액티비티 구현완료

--- app/src/main/java/com/devaon/early_buddy_android/feature/place_select_list/PlaceSelectActivity.kt
+++ app/src/main/java/com/devaon/early_buddy_android/feature/place_select_list/PlaceSelectActivity.kt
@@ -1,27 +1,27 @@
-package com.devaon.early_buddy_android.feature.place_search_list
+package com.devaon.early_buddy_android.feature.place_select_list
 
-import android.os.Bundle
-import android.util.Log
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
 import com.devaon.early_buddy_android.R
 import com.devaon.early_buddy_android.data.place.GetPlaceData
-import kotlinx.android.synthetic.main.activity_place_search.*
+import kotlinx.android.synthetic.main.activity_place_select.*
 
-class PlaceSearchActivity : AppCompatActivity() {
 
-    private lateinit var placeSearchAdapter: PlaceSearchAdapter
+class PlaceSelectActivity : AppCompatActivity() {
+
+    private lateinit var placeSelectAdapter: PlaceSelectAdapter
     private var placeDataList = ArrayList<GetPlaceData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_place_search)
+        setContentView(R.layout.activity_place_select)
 
         initPlaceSearchList()
     }
 
     private fun initPlaceSearchList() {
-        placeSearchAdapter = PlaceSearchAdapter(this).apply {
+        placeSelectAdapter = PlaceSelectAdapter(this).apply {
             data = listOf(
                 GetPlaceData("만수주공10단지아파트 1001동", "인천 남동구 만수동 1043 만수주공10단지아파트 1001", "장승남로 34"),
                 GetPlaceData("만수주공10단지아파트 1002동", "인천 남동구 만수동 1043 만수주공10단지아파트 1002", "장승남로 34"),
@@ -32,17 +32,17 @@ class PlaceSearchActivity : AppCompatActivity() {
             )
         }
         //test용 데이터
-        act_place_search_rv.adapter = placeSearchAdapter
+        act_place_select_rv.adapter = placeSelectAdapter
     }
 
     private fun setData() {
-        //데이버 받아오는 함수
+        //데이터 받아오는 함수
         if (placeDataList.isNullOrEmpty()) {
-            act_place_search_iv_bird.visibility = View.VISIBLE
-            act_place_search_rv.visibility = View.GONE
+            act_place_select_iv_bird.visibility = View.VISIBLE
+            act_place_select_rv.visibility = View.GONE
         } else {
-            act_place_search_iv_bird.visibility = View.GONE
-            act_place_search_rv.visibility = View.VISIBLE
+            act_place_select_iv_bird.visibility = View.GONE
+            act_place_select_rv.visibility = View.VISIBLE
 
             //통신할 때 데이터 받아와야함  data =
 
