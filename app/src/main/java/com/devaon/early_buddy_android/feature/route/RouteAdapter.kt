cd6620c03a1bec22efbb8ada1f5c08e9ce865b28
ghkdua1829 <ghkdua059@gmail.com>
2019-12-28 01:24:35 +0900

    [홈뷰제작]걷기 뷰  리사이클러뷰에 넣기

--- app/src/main/java/com/devaon/early_buddy_android/feature/route/RouteAdapter.kt
+++ app/src/main/java/com/devaon/early_buddy_android/feature/route/RouteAdapter.kt
@@ -14,7 +14,7 @@ class RouteAdapter(
     var context: Context,
     var clicked: ArrayList<Boolean>,
     var routeList: ArrayList<ArrayList<String>>
-) : RecyclerView.Adapter<RouteViewHolder>() {
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private lateinit var routeDetailAdapter: RouteDetailAdapter
 
@@ -22,27 +22,45 @@ class RouteAdapter(
         return routeList.size
     }
 
-    override fun onBindViewHolder(holder: RouteViewHolder, position: Int) {
-        holder.dropDown.setOnClickListener {
-            if (clicked[position]) {
-                holder.detailList.visibility = View.GONE
-                holder.dropDownUp.setImageResource(R.drawable.ic_dropbox_down)
-                clicked[position] = false
-            } else {
-                routeDetailAdapter = RouteDetailAdapter(routeList[position])
-                holder.detailList.visibility = View.VISIBLE
-                holder.detailList.adapter = routeDetailAdapter
-                holder.detailList.layoutManager = LinearLayoutManager(context)
-                holder.dropDownUp.setImageResource(R.drawable.ic_dropbox_up)
-                clicked[position] = true
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if(holder is RouteViewHolder){
+            holder.dropDown.setOnClickListener {
+                if (clicked[position]) {
+                    holder.detailList.visibility = View.GONE
+                    holder.dropDownUp.setImageResource(R.drawable.ic_dropbox_down)
+                    clicked[position] = false
+                } else {
+                    routeDetailAdapter = RouteDetailAdapter(routeList[position])
+                    holder.detailList.visibility = View.VISIBLE
+                    holder.detailList.adapter = routeDetailAdapter
+                    holder.detailList.layoutManager = LinearLayoutManager(context)
+                    holder.dropDownUp.setImageResource(R.drawable.ic_dropbox_up)
+                    clicked[position] = true
+                }
             }
         }
+        else{
+
+        }
+
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RouteViewHolder {
-        val view: View = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_pass_through_route, parent, false)
-        return RouteViewHolder(view)
+    override fun getItemViewType(position: Int): Int {
+        return position
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder{
+        return when(viewType){
+            0->{  val view: View = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_pass_through_route_first, parent, false)
+                FirstViewHolder(view)
+            }
+            else->{
+                val view: View = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_pass_through_route, parent, false)
+                RouteViewHolder(view)
+            }
+        }
     }
 }
 
@@ -51,4 +69,8 @@ class RouteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     var dropDown: ConstraintLayout = itemView.findViewById(R.id.item_pass_cl_drop_down_up)
     var detailList: RecyclerView = itemView.findViewById(R.id.item_pass_rv_riding_info_detail)
     var dropDownUp : ImageView= itemView.findViewById(R.id.item_pass_iv_drop_down_up_icon)
+}
+
+class FirstViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
 }
