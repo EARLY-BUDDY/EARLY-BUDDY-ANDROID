d0ece767e467e2c5a1f7ffa0677c8afad30ac2f7
ghkdua1829 <ghkdua059@gmail.com>
2019-12-31 01:31:28 +0900

    [홈뷰]타이머 줄이기 기능

--- app/src/main/java/com/devaon/early_buddy_android/feature/home/HomeActivity.kt
+++ app/src/main/java/com/devaon/early_buddy_android/feature/home/HomeActivity.kt
@@ -12,15 +12,20 @@ import com.devaon.early_buddy_android.feature.util.TextViewIntAnimation
 import com.devaon.early_buddy_android.feature.calendar.CalendarActivity
 import com.devaon.early_buddy_android.feature.route.RouteActivity
 import kotlinx.android.synthetic.main.activity_home.*
+import java.util.*
+import kotlin.concurrent.timer
 
 class HomeActivity : AppCompatActivity() {
 
-    var a = 0
+    private var time = 60000
+    private var timerTask: Timer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        act_home_tv_minute_number.setAnimInt(120)
+        act_home_tv_minute_number.setAnimInt(time)
+        start()
         changeColor()
         intent()
     }
@@ -53,4 +58,44 @@ class HomeActivity : AppCompatActivity() {
     fun TextView.setAnimInt(value: Int) {
         startAnimation(TextViewIntAnimation(this, to = value))
     }
+
+    private fun start() { //타이머 스타트
+
+        timerTask = timer(period = 10) {
+            // period = 10 0.01초 , period = 1000 면 1초
+            time--
+            // 0.01초마다 변수를 증가시킴
+
+            val hour = (time / 144000) % 24 // 1시간
+            val min = (time / 6000) % 60 // 1분
+            val sec = (time / 100) % 60 //1초
+            val milli = time % 100 // 0.01 초
+            runOnUiThread {
+                // Ui 를 갱신 시킴.
+
+                if (min < 10) { // 분
+                    act_home_tv_minute_number.text = "0$min"
+                } else {
+                    act_home_tv_minute_number.text = "$min"
+                }
+
+//                if (sec < 10) { // 초
+//                    act_home_tv_minute_number.text = "0$sec"
+//                } else {
+//                    act_home_tv_minute_number.text = "$sec"
+//                }
+////
+//                if (milli < 10){
+//                    act_home_tv_minute_number.text = "0$milli"
+//                }else {
+//                    act_home_tv_minute_number.text = "$milli"
+//                }
+
+                //$ 를 붙여주면 변하는 값을 계속 덮어준다
+                //ex) $를 붙여주면 기존에 1이라는 값이 잇을때 값이 2로변하면 2로 바꿔준다.
+
+            }
+        }
+    }
+
 }
