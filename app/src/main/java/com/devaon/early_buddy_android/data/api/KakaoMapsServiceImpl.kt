c36000088831bf35efa59cc28a16790d369ae7b2
devAon <yewon9742@gmail.com>
2019-12-31 16:40:26 +0900

    [로그인,최초가입]다이어로그

---
+++ app/src/main/java/com/devaon/early_buddy_android/data/api/KakaoMapsServiceImpl.kt
@@ -0,0 +1,19 @@
+package com.devaon.early_buddy_android.data.api
+
+import io.reactivex.schedulers.Schedulers
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+object KakaoMapsServiceImpl {
+    /*private const val BASE_URL = "https://dapi.kakao.com/"
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+        .client(httpClient.build())
+        .build()
+
+    val service: KakaoMapsService = retrofit.create(KakaoMapsService::class.java)*/
+}
