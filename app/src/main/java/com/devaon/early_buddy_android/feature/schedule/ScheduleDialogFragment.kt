63b28b3e55510c99d732fd78507bdb9501f32cbd
yejin <jinee0717@daum.net>
2020-01-02 01:04:37 +0900

    Schedule Popup 구현 완료! ㅎㅎ

---
+++ app/src/main/java/com/devaon/early_buddy_android/feature/schedule/ScheduleDialogFragment.kt
@@ -0,0 +1,26 @@
+package com.devaon.early_buddy_android.feature.schedule
+
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.DialogFragment
+import com.devaon.early_buddy_android.R
+
+class ScheduleDialogFragment : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.dialog_fragment_schedule_pop_up, container, false)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return super.onCreateDialog(savedInstanceState).apply {
+            window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        }
+    }
+}
