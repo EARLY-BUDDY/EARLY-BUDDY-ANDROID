0176a293d4cbf7fc07926d6b0d70578ee0049ab4
devAon <yewon9742@gmail.com>
2020-01-02 05:23:23 +0900

    [로그인,회원가입,최초가입]뷰연결

---
+++ app/src/main/java/com/devaon/early_buddy_android/data/user/PostUserData.kt
@@ -0,0 +1,4 @@
+package com.devaon.early_buddy_android.data.user
+
+class PostUserData {
+}
---
+++ app/src/main/java/com/devaon/early_buddy_android/feature/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.devaon.early_buddy_android.feature.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
