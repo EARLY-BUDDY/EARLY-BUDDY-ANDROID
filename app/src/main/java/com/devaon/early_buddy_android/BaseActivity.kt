d8b9484f95072313fcb607eaeb4076e7f0e53a08
yejin <jinee0717@daum.net>
2020-01-03 17:29:17 +0900

    [스플래시]완성

---
+++ app/src/main/java/com/devaon/early_buddy_android/BaseActivity.kt
@@ -0,0 +1,21 @@
+package com.devaon.early_buddy_android
+
+import androidx.appcompat.app.AppCompatActivity
+
+class BaseActivity : AppCompatActivity() {
+
+
+//    fun progressON() {
+//        EarlyBuddyApplication.getInstance().progressON(this, null)
+//        BaseApplication.get
+//    }
+//
+//    fun progressON(message: String) {
+//        BaseApplication.getInstance().progressON(this, message)
+//    }
+//
+//    fun progressOFF() {
+//        BaseApplication.getInstance().progressOFF()
+//    }
+
+}
